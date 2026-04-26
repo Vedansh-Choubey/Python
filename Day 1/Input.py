91916bbc9a9bf5d03528dbd5fbf1bb41d56fbc9f
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-26 22:40:59 +0530

    Fix comments in Input.py to use '#' instead of '//'

--- Day 1/Input.py
+++ Day 1/Input.py
@@ -1,6 +1,6 @@
-name = input ("Enter Your Name: ")     // String input 
+name = input ("Enter Your Name: ")     #String input 
 print ("Hello " , name)
 
-age = int (input("Enter Your Age: "))  // Integer input 
+age = int (input("Enter Your Age: "))  #Integer input (need to define the Input Type'int'
 print ("Your Are " , age ," Old")
 
