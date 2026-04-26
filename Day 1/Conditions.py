ae8e969c87694fa3220067c9e7a920142c14cdbc
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-26 23:19:04 +0530

    Add number condition checks in Conditions.py
    Fix conditional check for zero and improve output messages.

---
+++ Day 1/Conditions.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter Any Number: "))
+if num1 > 0:
+  print ("Its a Positive Number!!")
+elif num1 = 0:
+  print ("Entered Muber is Zero!!")
+else:
+  print ("Its a Negative Number ")
+
