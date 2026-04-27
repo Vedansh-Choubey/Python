503f9c87bb44f232374e59beff761baf52fb2e4a
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-27 23:01:58 +0530

    Refactor multiplication table to use user input
    Updated multiplication table logic to use user-defined number and range.

--- Day2/MultiplicationTable.py
+++ Day2/MultiplicationTable.py
@@ -1,3 +1,15 @@
-n = int (input("Enter Any Number: "))
+# Printing Multiplication table till 10
+
+n = int (input("Enter Any Number: "))    
 for i in range (1,11):
-  print ("5*", i , "=" , i*n)
+  print (n ,"*", i , "=" , i*n)
+
+
+
+#Printing Multiplication Table Upto The Value Entered By the user
+
+n = int (input("Enter Any Number: "))
+m = int (input("Enter the Range (upto): "))
+
+for i in range (1 , m+1):
+  print (n , "*" , i , "=" , i*n)
