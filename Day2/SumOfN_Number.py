74cbec3a87776598cd2fee15c6e7150e96d1dcfd
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-27 22:45:52 +0530

    Add program to calculate total and average marks
    Added a second practice program to calculate total and average marks.

--- Day2/SumOfN_Number.py
+++ Day2/SumOfN_Number.py
@@ -1,3 +1,5 @@
+# Practice Program 1
+
 total = 0
 n = int (input("Enter N Value: "))
 for i in range (1, n+1 ):           #Only n will lead to the (n-1)th value and the value n will be missed
@@ -6,6 +8,15 @@ for i in range (1, n+1 ):           #Only n will lead to the (n-1)th value and t
 print ("Sum Of N Numbers will be: ", total)
 
 
+# Practice Program 2
 
+total = 0
+subjects = int (input("Enter Your Subject Counts: "))
 
+for i in range (1 , subjects +1):
+    sub = int( input ("Enter Achieved marks out of 100: ")
+    total += sub
+
+print("Your Total Marks: " , total ," Out Of " , subjects *100)
+print("Your Average Marks: ", total / subjects)
 
