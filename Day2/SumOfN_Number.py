65eb5001ede8c1cb50af326eb3446a3d681a461a
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-27 22:30:16 +0530

    Fix comment clarity in SumOfN_Number.py

---
+++ Day2/SumOfN_Number.py
@@ -0,0 +1,11 @@
+total = 0
+n = int (input("Enter N Value: "))
+for i in range (1, n+1 ):           #Only n will lead to the (n-1)th value and the value n will be missed
+    total += i
+
+print ("Sum Of N Numbers will be: ", total)
+
+
+
+
+
