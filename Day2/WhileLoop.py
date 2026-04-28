c6b02e25a56b694f059f5a8ede427c29abc9c1b9
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-28 23:28:39 +0530

    Create WhileLoop.py

---
+++ Day2/WhileLoop.py
@@ -0,0 +1,15 @@
+#Predefine Value
+
+i = 1
+while i <= 5:
+  print("hello")
+  i += 1
+
+#User Input
+
+n =  input("Enter the Message: ")
+freq = int (input("Enter frequency of the message: ") )
+i = 1
+while i <= freq:
+  print (n)
+  i += 1
