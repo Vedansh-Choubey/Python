543659d53426cb5ffa73648caf079da23daa84b5
Vedansh choubey <190054585+Vedansh-Choubey@users.noreply.github.com>
2026-04-28 23:15:24 +0530

    Implement user input for factorial calculation
    Added user input functionality to calculate factorial.

--- Day2/Factorial.py
+++ Day2/Factorial.py
@@ -4,3 +4,11 @@ for i in range (1 , n+1):
   fact *= i
 
 print("Factorial Of Number-" , n ,"is equal to:", fact)
+
+#user input 
+n = int (input("Enter Any Number To Calculate their Factorial: "))
+fact = 1
+for i in range (1, n+1):
+  fact *= i 
+
+print("Factorial Of Number-" , n ,"is equal to:", fact)
